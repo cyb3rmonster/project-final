fb6bcc68976929e7b8aaa4ed0262e2e95f91ae52
elefante <elefante@KGS.local>
2022-11-26 14:00:33 -0500

    cookie cutter and venv

---
+++ icu_outcome_prediction/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='This is a projects help clinicians predict the out come of a patient admited to the ICU unit. The prediction is based on the lab work, medical history and basic vital signs.',
+    author='Ivan et Bertrand',
+    license='MIT',
+)
